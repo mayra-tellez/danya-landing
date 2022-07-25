14d16e2b4a9d8ed0a5c2be6520dda88ef2b2b181
mayrant <mayrant2004@yahoo.com>
2022-07-25 15:24:22 -0500

    created nav component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import Nav from './Components/Nav';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <div>Danya Tellez</div>
+        <Nav></Nav>
       </header>
     </div>
   );
---
+++ src/Components/Nav.js
@@ -0,0 +1,22 @@
+function Nav() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a>Purpose</a>
+        </li>
+        <li>
+          <a>Training</a>
+        </li>
+        <li>
+          <a>Arbonne</a>
+        </li>
+        <li>
+          <button>Get in Touch</button>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
