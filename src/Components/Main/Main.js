f3dc51781bec2cf6a29d377a3425b5641dfff25e
mayrant <mayrant2004@yahoo.com>
2022-07-25 23:41:22 -0500

    uploaded offerings icons

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,5 +1,11 @@
-import "./Main.css";
 import profilePic from "../../images/profilePic.png";
+import yogaIcon from "../../images/yogaIconTeal.svg";
+import fitnessIcon from "../../images/fitnessIconTeal.svg";
+// import nutritionIcon from "../../images/nutritionIcon.svg";
+// import wellnessIcon from "../../images/wellnessIcon.svg";
+import baristaIcon from "../../images/baristaIcon.svg";
+
+import "./Main.css";
 
 function Main() {
   return (
@@ -16,17 +22,17 @@ function Main() {
         <h2>Wellness Your Way / Your Way to Wellness</h2>
         <div className="offerings-container">
           <article>
-            <img src="" alt="yoga icon" />
+            <img className="offerings-icons" src={yogaIcon} alt="yoga icon" />
             <h4>Yoga</h4>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vel blandit nulla.</p>
           </article>
           <article>
-            <img src="" alt="fitness icon" />
+            <img className="offerings-icons" src={fitnessIcon} alt="fitness icon" />
             <h4>Fitness</h4>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vel blandit nulla.</p>
           </article>
           <article>
-            <img src="" alt="nutrition icon" />
+            <img className="offerings-icons" src={baristaIcon} alt="nutrition icon" />
             <h4>Nutrition</h4>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vel blandit nulla.</p>
           </article>
